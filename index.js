2e489bd08eb668f053ba5c103df39ab2745fe1a3
Danny Bauman <dannybauman@gmail.com>
2015-03-20 14:37:22 -0700

    Add beginnings of simple-peer connections, simplify code

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Firebase = require('firebase')
+var simplepeer = require('simple-peer')
 
 module.exports = FireFlower
 
@@ -15,36 +16,36 @@ function FireFlower (firebaseUrl, k, peerId) {
   this.k = k
   this.numSubscribers = 0
   this.myPeerId = peerId
-  this.broadcasterId = null
+  this.peers = {}
 }
 
 FireFlower.prototype.setBroadcaster = function () {
-  this.broadcasterId = this.myPeerId
   var broadcasterRef = this.firebase.child('available_peers/' + this.myPeerId)
-  // create the node in Firebase
+  // create an entry for this broadcaster in Firebase's list of available peers
   broadcasterRef.set({id: this.myPeerId}, function (err) {
     if (err) {
       throw err
     }
   })
 
-  // todo: make this work if there are already subscribers waiting
+  // todo: if there are any existing peers connected and
+  // waiting, we should start the process of connecting
+  // everyone
 }
 
 FireFlower.prototype.subscribe = function () {
   var self = this
-  findPeerWithAvailableSlot.call(this, function (availablePeerSnapshot) {
-    var availablePeerId = availablePeerSnapshot.val().id
-    connectToPeer.call(self, availablePeerId, self.myPeerId)
+  findPeerWithAvailableSlot.call(this, function (availablePeerId) {
+    startListening.call(self, availablePeerId)
   })
-}
-
-FireFlower.prototype.setAsUnavailable = function () {
-  this.firebase.child('available_peers/' + this.myPeerId).remove()
 }
 
 FireFlower.prototype.setAsAvailable = function () {
   this.firebase.child('available_peers/' + this.myPeerId).set({id: this.myPeerId})
+}
+
+FireFlower.prototype.setAsUnavailable = function () {
+  this.firebase.child('available_peers/' + this.myPeerId).remove()
 }
 
 function findPeerWithAvailableSlot (cb) {
@@ -53,39 +54,115 @@ function findPeerWithAvailableSlot (cb) {
     snapshot.forEach(function (childSnapshot) {
       // only consider this peer if it isn't ourself
       if (childSnapshot.val().id !== self.myPeerId) {
-        cb(childSnapshot)
+        cb(childSnapshot.val().id)
         return true
       }
     })
   })
 }
 
-function connectToPeer (upstreamPeerId, downstreamPeerId) {
-  console.log('connecting upstream peer ' + upstreamPeerId + ' to downstream peer ' + downstreamPeerId)
-  // todo: implement data channel connection
-  // todo: when data connection is lost, make sure to remove
-  //       this downstream peer ID from this upstream peer's
-  //       list of subscribers
+function startListening (upstreamPeerId) {
+  var self = this
 
-  // on connect
-  if (upstreamPeerId === this.myPeerId) {
-    this.numSubscribers++
-    if (this.numSubscribers >= this.k) {
-      this.setAsUnavailable()
-    }
-  } else if (downstreamPeerId === this.myPeerId) {
-    // now that we've connected to an upstream peer,
-    // add ourself to the pool of avaialble peers
-    this.setAsAvailable.call(this)
+  // if there is no listener data, or that listener is this user,
+  // then ignore it
+  if (!upstreamPeerId || upstreamPeerId === this.myPeerId) {
+    return
   }
 
-  // on disconnect
-  if (upstreamPeerId === this.myPeerId) {
-    removeSubscriber.call(this)
+  var signalRef = this.firebase.child('signals/' + this.myPeerId)
+
+  signalRef.set({
+    peer_id: this.myPeerId
+  }, function (err) {
+    if (err) {
+      return signalRef.once('value', startListening.bind(self))
+    }
+
+    var peer = self.peers[upstreamPeerId]
+
+    if (peer && !peer.destroyed) {
+      // if we already know about this peer, try to renegotiate -
+      // the other side is responsible for tearing down the connection
+      // so we wait here and flag that a renegotiation is in needed
+      peer.needsRenegotiation = true
+      peer.once('close', function () {
+        connectToPeer.call(self, true, upstreamPeerId)
+      })
+      return
+    }
+
+    // push a new child on the upstream peer's list of signals,
+    // which should cause them to attempt to connect to us as well
+    var upstreamSignalRef = self.firebase.child('signals/' + upstreamPeerId)
+    upstreamSignalRef.push({id: self.myPeerId})
+
+    connectToPeer.call(self, true, upstreamPeerId)
+  })
+}
+
+function connectToPeer (initiator, destinationPeerId) {
+  var self = this
+  var timeout = null
+  var localSignals = this.firebase.child('signals').child(this.myPeerId)
+  var remoteSignals = this.firebase.child('signals').child(destinationPeerId)
+
+  var peer = simplepeer({
+    initiator: initiator
+  })
+
+  this.peers[destinationPeerId] = peer
+
+  peer.on('signal', function (signal) {
+    signal = JSON.parse(JSON.stringify(signal))
+    localSignals.push(signal, logerror)
+  })
+
+  peer.on('connect', function () {
+    clearTimeout(timeout)
+    peer.removeAllListeners('signal')
+    peer.removeAllListeners('connect')
+    remoteSignals.off()
+
+    if (initiator) {
+      if (self.output) {
+        self.output.pipe(peer)
+      }
+    } else if (!self.output) {
+      self.output = peer
+    }
+  })
+
+  peer.on('close', function () {
+    clearTimeout(timeout)
+    window.removeEventListener('beforeunload', onbeforeunload)
+    peer.removeAllListeners()
+    remoteSignals.off()
+    delete self.peers[destinationPeerId]
+
+    if (self.output) {
+      self.output.unpipe(peer)
+      if (self.output === peer) {
+        self.output = null
+      }
+    }
+  })
+
+  remoteSignals.on('child_added', function (signal) {
+    peer.signal(signal.val().id)
+  })
+
+  timeout = setTimeout(function () {
+    peer.destroy()
+  }, 10000)
+
+  window.addEventListener('beforeunload', onbeforeunload)
+
+  function onbeforeunload () {
+    peer.destroy()
   }
 }
 
-function removeSubscriber () {
-  this.numSubscribers--
-  this.setAsAvailable.call(this)
+function logerror (err) {
+  if (err) return console.error(err)
 }
