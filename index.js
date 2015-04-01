4b1fe0142735ed67211ad5569653a4f3f33a0508
Danny Bauman <dannybauman@gmail.com>
2015-04-01 14:58:31 -0700

    don't cleanup log, do record timestamp

--- index.js
+++ index.js
@@ -109,8 +109,6 @@ Node.prototype.disconnect = function () {
 
   // stop any reporting that may have been happening
   clearInterval(this._interval)
-  // remove this node's report node completely
-  this._logsRef.child(this.id).remove()
 
   // teardown listeners
   this.removeListener('configure', this._doconnect)
@@ -513,7 +511,8 @@ function generateNodeStatusObject () {
 
   return {
     id: this.id,
-    upstream_peer_id: upstreamPeerId
+    upstream_peer_id: upstreamPeerId,
+    timestamp: new Date().getTime()
   }
 }
 
