cc1ec5d046136afce606b50b66779d8fbeba5348
Danny Bauman <dannybauman@gmail.com>
2015-04-18 09:20:43 -0700

    fixed merged extra text

--- example/index.js
+++ example/index.js
@@ -1,30 +1,10 @@
 var fireflower = require('../')(require('firebase'))
 var Graph = require('./src/graph')
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 window.root = fireflower('fireflower.firebaseio.com', { root: true })
 window.root.connect()
-=======
-window.root = fireflower('fireflower-dev.firebaseio.com', { id: '0' })
-window.root.connect(true)
->>>>>>> stub for reporting status every 5 seconds if the node opts in
-=======
-window.root = fireflower('fireflower-dev.firebaseio.com', { id: '0' })
-window.root.connect(true)
->>>>>>> stub for reporting status every 5 seconds if the node opts in
-=======
-window.root = fireflower('fireflower-dev.firebaseio.com', { id: '0' })
-window.root.connect(true)
->>>>>>> stub for reporting status every 5 seconds if the node opts in
-=======
-window.root = fireflower('fireflower-dev.firebaseio.com', { id: '0' })
-window.root.connect(true)
->>>>>>> 5635abd521ff61c8b47e68ab2748bad3999276f3
 
 window.root.once('connect', function () {
-  window.graph = new Graph('fireflower-dev.firebaseio.com', window.root)
+  window.graph = new Graph('fireflower.firebaseio.com', window.root)
   window.graph.render()
 })
